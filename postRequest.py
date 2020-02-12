b0fe2b91281308d36bfc8544fc4d8aaa2b386d30
CynthiaCalixtro <ccalixtroa@uni.pe>
2020-02-12 16:25:49 -0500

    cambios necesarios para trabajar con python3

--- postRequest.py
+++ postRequest.py
@@ -2,7 +2,7 @@ import datetime
 import requests
 
 module_name = 'MOD3'
-orion_server = 'http://143.137.145.12:1026'#'http://190.119.192.192:1026' 
+orion_server = 'http://190.119.192.192:1026' #'http://143.137.145.12:1026'#
 prefix = '/v2/entities/urn:ngsi-ld:DataObserved:'+module_name+'/attrs'  #DIRECCION SERVIDOR
 url_server = orion_server+prefix
 
@@ -13,4 +13,4 @@ def datenow():
 
 def post(data):
     r = requests.post(url_server, json=data)
-    print r.status_code,r.reason
+    return r.status_code,r.reason
