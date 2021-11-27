218c3e9298d2b579891ba44d9bd6f64408955e62
DariAcuna <dacunaq@uni.pe>
2021-11-27 16:12:44 -0500

    added filters to __read method in Sensor class

--- utils/Sensor.py
+++ utils/Sensor.py
@@ -6,13 +6,33 @@ import time
 class Sensor:
 
     def __init__(self, port):
+        # default values match suggested paramaters
+        # baudrate=9600         ->  Baud: 9600
+        # bytesize=EIGHTBITS    ->  Data bits: 8
+        # stopbits=STOPBITS_ONE ->  Stop bits: 1
+        # parity=PARITY_NONE    ->  Parity: None
+        # xonxoff=False         ->  Flow Control: None
+        # adding timeout to avoid strokes
         self.port = serial.Serial('/dev/ttyUSB0')
 
     # private method
     def __read(self):
         ser_bytes = self.port.readline()
+        time.sleep(1)
         decoded_bytes = ser_bytes[0:len(ser_bytes) - 2].decode("utf-8")
-        return decoded_bytes
+
+        # if received ghost data, skip
+        if not decoded_bytes:
+            return
+
+        decoded_list = list(decoded_bytes.split(","))
+        available_list = [int(item) for item in decoded_list]
+
+        # if received incomplete data, skip
+        if not len(available_list) == 11:
+            return
+
+        return available_list
 
     # sends a key to trigger a measurement
     def __trigger(self):
