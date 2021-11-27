cad7759c9f9dad13b214d10f05418fc96d293b12
DariAcuna <dacunaq@uni.pe>
2021-11-27 14:05:41 -0500

    fixed unexpected reading stroke and patched initial empty read string

--- serialRead2.py
+++ serialRead2.py
@@ -7,42 +7,58 @@ import time
 # stopbits=STOPBITS_ONE ->  Stop bits: 1
 # parity=PARITY_NONE    ->  Parity: None
 # xonxoff=False         ->  Flow Control: None
-serialPort = serial.Serial('/dev/ttyUSB0')
+serialPort = serial.Serial('/dev/ttyUSB0', timeout=1)
 print('\nSerial information:\n' + str(serialPort) + '\n')
 
 # clean buffers
 print('Cleaning buffers...\n')
-time.sleep(1)
-serialPort.reset_output_buffer()
-time.sleep(1)
-serialPort.reset_input_buffer()
+# time.sleep(1)
+# serialPort.reset_output_buffer()
+time.sleep(3)
+# serialPort.reset_input_buffer()
 
-# set delay
-time.sleep(2)
 # sends a key to trigger a measurement
 dataTrigger = 't'.encode()
 serialPort.write(dataTrigger)
+serialPort.flush()
 # sets delay to make sure character 't' is read
-time.sleep(2)
+
+# within 5-seconds of a TRIGGER key stroke
+time.sleep(3)
+
 # starts continuous measurement
 dataOutput = 'c'.encode()
 serialPort.write(dataOutput)
+serialPort.flush()
+serialPort.write(dataOutput)
+serialPort.flush()
 
-# TODO: fix data reading inconsistency (ghost data)
+time.sleep(5)
 
 try:
     while True:
         # wait until there is data waiting in the serial buffer
+
         if serialPort.in_waiting > 0:
 
             ser_bytes = serialPort.readline()
+            time.sleep(1)
             decoded_bytes = ser_bytes[0:len(ser_bytes) - 2].decode("utf-8")
+
+            if not decoded_bytes:
+                continue
+
             print(decoded_bytes)
 
+        serialPort.flush()
+
 except KeyboardInterrupt:
+    time.sleep(3)
+    serialPort.flush()
+    time.sleep(1)
     dataStop = 'R'.encode()
     serialPort.write(dataStop)
     serialPort.flush()
     print('\nStopping continuous data output stream')
-    time.sleep(2)
+    time.sleep(3)
     pass
