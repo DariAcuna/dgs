200619aea75b02912557e344472f4c72502293ef
DariAcuna <dacunaq@uni.pe>
2021-11-29 16:08:34 -0500

    added ports module: identifies what port corresponds to each sensor

---
+++ ports.py
@@ -0,0 +1,21 @@
+from utils.sensor import Sensor
+import time
+
+# output from ports.sh
+ports = [0, 1]
+sensors = []
+
+# opens ports
+for port in ports:
+    sensors.append(Sensor(port=port))
+
+# allow the port to open
+time.sleep(3)
+
+gases = []
+
+for sensor in sensors:
+    gases.append(sensor.gas())
+    sensor.ser.flush()
+
+print(gases)
