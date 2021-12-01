9b64f91f732381d99611a4936dcd4e0b615c71ec
DariAcuna <dacunaq@uni.pe>
2021-12-01 15:20:11 -0500

    added private method eeprom: reads eeprom data from dgs

--- utils/sensor.py
+++ utils/sensor.py
@@ -25,21 +25,67 @@ class Sensor:
             return
 
         decoded_list = list(decoded_bytes.split(","))
+
+        # check if item in list is empty
+        for item in decoded_list:
+            if not item:
+                return
+
+        # accidentally reading eeprom?
         available_list = [int(item) for item in decoded_list]
 
         # if received incomplete data, skip
         if not len(available_list) == 11:
-            print('invalid')
             return
 
         return available_list
 
     # private method
     # TODO: write method that gets all lines from EEPROM readout
+    def __eeprom(self):
+
+        lines = []
+        counter = 0
+        breakline = '\n'.encode()
+        lenterm = len(breakline)
+
+        for i in range(20):
+            lines.append(bytearray())
+
+        line = lines[0]
+        while True:
+            c = self.ser.read(1)
+            if c:
+                line += c
+                if line[-lenterm:] == breakline:
+                    counter += 1
+                    lines[counter-1] = line
+                    if counter == 20:
+                        break
+                    line = lines[counter]
+            else:
+                break
+
+        eeprom = [bytes(line) for line in lines]
+        return eeprom
+
+    def __gas(self):
+        # gases = {'H2S', 'NO2', 'SO2', 'O3', 'CO'}
+
+        ser_bytes = self.__eeprom()[17]
+        time.sleep(1)
+        gas = ser_bytes[0:len(ser_bytes)].decode("utf-8")
+
+        # get gas type from eeprom
+        # gas = decoded_bytes.split('\n', 18)[17]
+        gas_raw = gas.split(' ', 1)[1]
+        gas_type = gas_raw[:-2]
+
+        return gas_type
 
     # private method
     # TODO: get gas code from method above
-    def __eeprom(self):
+    def __temp_gas(self):
         # gases = {'H2S', 'NO2', 'SO2', 'O3', 'CO'}
 
         ser_bytes = self.ser.read_until('Date_Code'.encode())
@@ -60,7 +106,7 @@ class Sensor:
         self.ser.write(trigger)
         self.ser.flush()
 
-        # within 5-seconds of a TRIGGER key stroke
+        # within 5-seconds of a TRIGGER keystroke
         time.sleep(3)
         return
 
@@ -84,7 +130,7 @@ class Sensor:
 
     def gas(self):
         self.__send('e')
-        gas = self.__eeprom()
+        gas = self.__gas()
         return gas
 
     # TODO: test calibrate method
